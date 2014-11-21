fdd32d94c12b44ffaa3b024b131447bb8341ae55
Coder Vox <codervox1@Coder-Voxs-Mac-mini.local>
2014-11-20 18:55:16 -0600

    first commit

---
+++ app/config/packages/pingpong/twitter/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+	'consumer_key'		=>	'mbhxCseUprBbO49TZ5Ee3XJIM',
+	'consumer_secret'	=>  'w57Vz4a5HJv5W5Tqe54BgBgiCOUnwQS2pasb8JdFeOe0ptGT6I',
+	
+	'oauth_token'		=>	null,
+	'oauth_token_secret'=>  null,
+
+	'bearer_token'		=>	null,
+	
+	'callback_url'		=>  url('twitter/callback'),
+    'fallback_url'      =>  url('/')
+);
