2f468b03b1588d3d20c9102dca4a4d382d72c235
Coder Vox <codervox1@Coder-Voxs-Mac-mini.local>
2014-11-20 18:56:45 -0600

    first commit

---
+++ vendor/pingpong/twitter/src/Pingpong/Twitter/Traits/StreamingTrait.php
@@ -0,0 +1,32 @@
+<?php namespace Pingpong\Twitter\Traits;
+
+// TODO : Add streaming feature
+
+trait StreamingTrait {
+
+    public function postStatusesFilter()
+    {
+        
+    }
+
+    public function getStatusesSample()
+    {
+        
+    }
+
+    public function getStatusesFirehose()
+    {
+        
+    }
+
+    public function getUser()
+    {
+        
+    }
+
+    public function getSite()
+    {
+
+    }
+
+} 
